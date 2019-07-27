fc06a009e44a770cdc14e2e59c0a5fbcfcc89b70
Gleizer Freitas <gleizerf@gmail.com>
2019-07-26 22:44:59 -0300

    Add tasks to observe changes on scss and pub sources and reload server

--- app/templates/Gulpfile.js
+++ app/templates/Gulpfile.js
@@ -1,11 +1,58 @@
 'use strict';
 
-const { series, parallel } = require('gulp');
-const { src, dest } = require('gulp');
+const { series, parallel, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+const server = require('browser-sync').create();
 const pug = require('gulp-pug');
 const tinify = require('gulp-tinify');
+
+const paths = {
+  styles: {
+    src: 'src/scss/*.scss',
+    dest: 'dist/css/'
+  },
+  docs: {
+    src: 'src/pug/*.pug',
+    srcWatch: 'src/pug/**/*.pug',
+    dest: 'dist/'
+  },
+  images: {
+    src: 'src/img/*',
+    dest: 'dist/img/'
+  }
+};
+
+function reload(done) {
+  server.reload();
+  done();
+}
+
+function serve(done) {
+  server.init({
+    server: {
+      baseDir: 'dist/'
+    }
+  });
+  done();
+}
+
+function cssTranspile() {
+	return src(paths.styles.src)
+	  .pipe(sass().on('error', sass.logError))
+    .pipe(dest(paths.styles.dest));
+}
+
+function imgTinify() {
+  return src(paths.images.src)
+    //.pipe(tinify('<%= tinifyAPIKey %>'))
+    .pipe(dest(paths.images.dest));
+}
+
+function pugCompile() {
+  return src(paths.docs.src)
+    .pipe(pug())
+    .pipe(dest(paths.docs.dest));
+}
 
 function cpLibElegantIcons() {
   return src('node_modules/elegant-icons/**/*')
@@ -18,38 +65,19 @@ function cpLibFlexBoxGrid() {
     .pipe(dest('dist/lib/flexboxgrid'));
 }
 
-function cssTranspile() {
-	return src('src/scss/*.scss')
-	  .pipe(sass().on('error', sass.logError))
-    .pipe(dest('dist/css'));
-}
-
-function imgTinify() {
-  return src('src/img/*')
-    //.pipe(tinify('<%= tinifyAPIKey %>'))
-    .pipe(dest('dist/img'));
-}
-
-function pugCompile() {
-  return src('src/pug/*.pug', {allowEmpty: true})
-    .pipe(pug())
-    .pipe(dest('dist'));
-}
-
-function publish() {
-  browserSync.init({
-		server: {
-			baseDir: 'dist'
-		}
-	});
+function observe() {
+  watch(paths.styles.src, series(cssTranspile, reload));
+  watch(paths.docs.srcWatch, series(pugCompile, reload));
 }
 
 exports.build = series(
-  cpLibElegantIcons,
-  cpLibFlexBoxGrid,
-  imgTinify,
   parallel(
+    cpLibElegantIcons,
+    cpLibFlexBoxGrid),
+  parallel(
+    imgTinify,
     cssTranspile,
     pugCompile),
-  publish
+  serve,
+  observe
 );
