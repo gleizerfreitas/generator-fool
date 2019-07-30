dce0cc4069f31b749b52a60e197b138708db14a8
Gleizer Freitas <gleizerf@gmail.com>
2019-07-30 00:12:09 -0300

    control menu

---
+++ app/templates/src/js/app.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const nav = document.querySelector("#nav-main");
+const menu_toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+
+//toggle menu
+var navToggle = function() {
+    menu.classList.toggle("active");
+    nav.classList.toggle("active");
+    menu_toggle.dataset.icon == "a" ? menu_toggle.dataset.icon = "M" : menu_toggle.dataset.icon = "a";    
+}
+menu_toggle.addEventListener("click", navToggle);
+
+//collapse menu on resize
+var navCollapse = function() {
+    if(menu.classList.contains("active")) 
+        navToggle();
+}
+window.onresize = navCollapse;
+
+//shrink nav
+var navShrink = function() {
+    nav.classList.toggle("shrink", document.scrollingElement.scrollTop > 100)
+};
+navShrink();
+window.onscroll = navShrink;
+
+// close menu when a #nav-main link is clicked
+const links = document.querySelectorAll(".logo, .menu a");
+links.forEach(function(i) {
+    i.addEventListener("click", navCollapse);
+});
+
